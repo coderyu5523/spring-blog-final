f38259a78c2f3f1444eed317734db7960947fe87
coderyu5523 <coderyu5523@naver.com>
2024-03-20 18:42:04 +0900

    board 엔티티

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -1,4 +1,21 @@
 package shop.mtcoding.blog.board;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="board_tb")
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id ;
+    private String title;
+    private String content;
+    private int userId ;
+    private LocalDateTime createdAt;
+
 }
