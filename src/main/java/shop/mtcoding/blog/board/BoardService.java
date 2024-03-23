9d65d73564f5519cff475dd2c5ed4413a6662938
coderyu5523 <coderyu5523@naver.com>
2024-03-23 20:31:54 +0900

    게시글 출력

--- src/main/java/shop/mtcoding/blog/board/BoardService.java
+++ src/main/java/shop/mtcoding/blog/board/BoardService.java
@@ -1,7 +1,19 @@
 package shop.mtcoding.blog.board;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class BoardService {
+
+    private final BoardJPARepository boardJPARepository ;
+
+    public List<Board> findAll() {
+       List<Board> boardList = boardJPARepository.findAll(Sort.by(Sort.Direction.DESC,"id"));
+       return boardList;
+    }
 }
